9909f3641dc0d10dbab6d2363ce574bfb7210384
ButterHost69 <butterhost069@gmail.com>
2025-02-14 01:21:55 +0530

    PING Method Added

--- server/handlers.go
+++ server/handlers.go
@@ -1,6 +1,9 @@
 package server
 
-import "go.uber.org/zap"
+import (
+	"github.com/ButterHost69/PKr-Server/db"
+	"go.uber.org/zap"
+)
 
 type Handler struct {
 	sugar 	*zap.SugaredLogger
@@ -8,5 +11,19 @@ type Handler struct {
 
 func (h *Handler) Ping(req PingRequest, res *PingResponse)(error){
 
+	if req.Username == "" {
+		res.Response = 203
+	}
+
+	if req.PublicIP == "" || req.PublicPort == ""{
+		res.Response = 205
+	}
+
+	if err := db.UpdateUserIP(req.Username, req.Password, req.PublicIP, req.PublicPort); err != nil {
+		res.Response = 203
+		h.sugar.Errorf("Could Not Update IP For User %s : Err: %v", req.Username, err)
+	}
+
+	res.Response = 200
 	return nil
 }
--- server/models.go
+++ server/models.go
@@ -2,10 +2,13 @@ package server
 
 
 type PingRequest struct {
+	PublicIP	string
+	PublicPort	string
+
 	Username	string
 	Password	string
 }
 
 type PingResponse struct {
-
+	Response	int
 }
