709dac56436758a4fcb6a1c3e94d0401285e2486
ButterHost69 <butterhost069@gmail.com>
2025-06-10 22:44:17 +0530

    merge moit

--- server/server.go
+++ server/server.go
@@ -162,7 +162,14 @@ func InitServer(port string, sugar *zap.SugaredLogger) error {
 		remoteAddr := session.RemoteAddr().String()
 		sugar.Infof("New incoming connection from %s", remoteAddr)
 		// go rpc.ServeConn(session)
+
 		go func(session *kcp.UDPSession) {
+			session.SetWindowSize(2, 32)                               // Only 2 unacked packets maximum
+			session.SetWriteDeadline(time.Now().Add(10 * time.Second)) // Limits total retry time
+			session.SetNoDelay(0, 15000, 0, 0)
+			session.SetDeadline(time.Now().Add(20 * time.Second)) // Overall timeout
+			session.SetACKNoDelay(false)                          // Batch ACKs to reduce traffic
+
 			sugar.Info("Creating CustomServeCodec for Session - ", remoteAddr)
 			customCodec := NewCustomServeCodec(session, sugar)
 			sugar.Info("Serving Connection to Codec for Session - ", remoteAddr)
@@ -174,11 +181,5 @@ func InitServer(port string, sugar *zap.SugaredLogger) error {
 			customCodec.Close()
 		}(session)
 		
-		session.SetWindowSize(2, 32)                               // Only 2 unacked packets maximum
-		session.SetWriteDeadline(time.Now().Add(10 * time.Second)) // Limits total retry time
-		session.SetNoDelay(0, 15000, 0, 0)
-		session.SetDeadline(time.Now().Add(20 * time.Second)) // Overall timeout
-		session.SetACKNoDelay(false)                          // Batch ACKs to reduce traffic
-		go rpc.ServeConn(session)
 	}
 }
