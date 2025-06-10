5b30002c2a8fef027c458014d9f6026b77534023
ButterHost69 <butterhost069@gmail.com>
2025-06-11 02:47:35 +0530

    idk wwhaa

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"bufio"
 	"encoding/gob"
+	"fmt"
 	"io"
 	"log"
 	"net"
@@ -108,17 +109,18 @@ func (c *CustomServeCodec) WriteResponse(r *rpc.Response, x interface{}) error {
 // Close can be called multiple times and must be idempotent.
 func (c *CustomServeCodec) Close() error {
 	c.sugar.Info("Closing RPC Connection for - ", c.conn.RemoteAddr().String())
-	// if err := c.defaultCodec.Close(); err != nil {
-	// 	return err
-	// }
+	if err := c.defaultCodec.Close(); err != nil {
+		return err
+	}
 
-	// if err := c.conn.Close(); err != nil {
-	// 	return fmt.Errorf("error in closing kcp session, err - %v", err)
-	// }
+	if err := c.conn.Close(); err != nil {
+		c.sugar.Debug("error in closing kcp session, err - %v", err)
+		return fmt.Errorf("error in closing kcp session, err - %v", err)
+	}
 
-	// return nil
+	return nil
 
-	return c.defaultCodec.Close()
+	// return c.defaultCodec.Close()
 }
 
 func InitServer(port string, sugar *zap.SugaredLogger) error {
@@ -153,6 +155,7 @@ func InitServer(port string, sugar *zap.SugaredLogger) error {
 		return err
 	}
 
+	session_ongoing := make(map[string]*CustomServeCodec)
 	sugar.Info("Started KCP Server...")
 	for {
 		session, err := lis.AcceptKCP()
@@ -162,14 +165,21 @@ func InitServer(port string, sugar *zap.SugaredLogger) error {
 		}
 		remoteAddr := session.RemoteAddr().String()
 		sugar.Infof("New incoming connection from %s", remoteAddr)
-		// go rpc.ServeConn(session)
+		
+		// Ik this is shit, but what can i do ....
+		if session_ongoing[remoteAddr] != nil{
+			sugar.Info("The Incoming connection from %s", remoteAddr, " is treated as response ack")
+			sugar.Info("Serve Codec Done Calling Close for Session - ", remoteAddr)
+
+			session_ongoing[remoteAddr].Close()
+		}
 
 		go func(session *kcp.UDPSession) {
 			session.SetWindowSize(2, 32)                               // Only 2 unacked packets maximum
 			session.SetWriteDeadline(time.Now().Add(10 * time.Second)) // Limits total retry time
 			// session.SetNoDelay(0, 15000, 0, 0)
 			session.SetNoDelay(1, 10, 0, 0)
-			session.SetDeadline(time.Now().Add(20 * time.Second)) // Overall timeout
+			session.SetDeadline(time.Now().Add(30 * time.Second)) // Overall timeout
 			// session.SetACKNoDelay(false)                          // Batch ACKs to reduce traffic
 			session.SetACKNoDelay(true)                          // Don't Batch ACKs to reduce traffic
 
@@ -180,8 +190,9 @@ func InitServer(port string, sugar *zap.SugaredLogger) error {
 			if err != nil {
 				sugar.Debugf("error in serving request to session: %s, error - %v", remoteAddr, err)
 			}
-			sugar.Info("Serve Codec Done Calling Close for Session - ", remoteAddr)
-			customCodec.Close()
+			sugar.Info("Response Sent 👍")
+
+			// customCodec.Close()
 		}(session)
 		
 	}
